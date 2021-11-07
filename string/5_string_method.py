6e5d6546ff3f39de31a166d24c852a9f70ccd4e7
Just-Hilman <64965507+Just-Hilman@users.noreply.github.com>
2021-11-07 11:15:45 +0700

    string method

---
+++ string/5_string_method.py
@@ -0,0 +1,23 @@
+# Upper & Lower
+"Mountains".upper();
+"MOUNTAINS".lower();
+
+#Strip, remove space
+" Yes ".strip();
+" Yes ".lstrip();
+" Yes ".rstrip();
+
+#Count, to count how many substring occurs in string
+"The number e occurs in this string is 3".count();
+
+#Endswith, to check last string contain some substring
+"Forest".endswith("rest");
+
+#isNumeric, to check string contain number
+"Forest".isnumeric();
+
+#Join
+" ".join(["This", "is", "the", "phrase", "joined"]);
+
+#Split
+"This is another example".split();
